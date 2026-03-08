8380480c56a9da284d5984065ff4505c193f679c
madraceee <raajanitheesh@gmail.com>
2026-03-08 16:42:26 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -9,6 +10,9 @@ int main(int argc, char *argv[]) {
 		// TODO: Uncomment the code below to pass the first stage
 		printf("$ ");
 		scanf("%s",input);
+		if(strcmp(input,"exit") == 0){
+			break;
+		}
 		printf("%s: command not found\n", input);
 	}
 
