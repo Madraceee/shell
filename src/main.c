223746a6fc2d21af7fb0ee9fe6a12965b402006e
madraceee <raajanitheesh@gmail.com>
2026-03-08 18:03:55 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
+	
+	char* path_value = getenv("PATH");
+	if(path_value == NULL){
+		return 1;
+	}
+	int no_of_path = 0;
+	for(int i=0; path_value[i] != '\0';i++){
+		if(path_value[i] == ':'){
+			no_of_path += 1;
+		}
+	}
+
+	char *paths[no_of_path];
+	int count = 0;
+	while(path_value != NULL){
+		char* path = strsep(&path_value, ":");
+		paths[count] = (char*)malloc(sizeof(char)* strlen(path));
+		strcpy(paths[count], path);
+		count += 1;
+	}
 
 	char cmds[3][5] = {
 		"echo",
@@ -30,6 +53,30 @@ int main(int argc, char *argv[]) {
 					isValid = 1;
 				}
 			}
+			if(isValid == 0){
+				for(int i=0;i<no_of_path;i++){
+					DIR *dir = opendir(paths[i]);
+					struct dirent *ent;
+					while((ent = readdir(dir))){
+						if(strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0){
+							continue;
+						}
+
+						char full_path[PATH_MAX];
+						snprintf(full_path, PATH_MAX, "%s/%s", paths[i], ent->d_name);
+						if(strcmp(input+5, ent->d_name) == 0 && access(full_path, X_OK) == 0){
+							printf("%s is %s\n", input+5, full_path);
+							isValid = 1;
+							break;
+						}
+					}
+
+					if(isValid == 1){
+						break;
+					}
+				}
+			}
+
 			if(isValid == 0){
 				printf("%s: not found\n",input+5);
 			}
