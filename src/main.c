2bdc4235c1b71e0c0a7bcc52e8540aa097df878d
madraceee <raajanitheesh@gmail.com>
2026-03-08 17:05:23 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
+
+	char cmds[3][5] = {
+		"echo",
+		"exit",
+		"type"
+	};
+	
   setbuf(stdout, NULL);
 	while(1){
 		char* input = (char*)malloc(sizeof(char) * 500);
@@ -15,6 +22,17 @@ int main(int argc, char *argv[]) {
 			break;
 		}else if(strncmp(input,"echo ",5) == 0){
 			printf("%s\n", input + 5);
+		}else if (strncmp(input,"type ",5) == 0) {
+			int isValid = 0;
+			for(int i=0;i<3;i++){
+				if(strncmp(input+5, cmds[i], strlen(cmds[i]) ) == 0) {
+					printf("%s is a shell builtin\n", cmds[i]);
+					isValid = 1;
+				}
+			}
+			if(isValid == 0){
+				printf("%s: not found",input+5);
+			}
 		}else {
 			printf("%s: command not found\n", input);
 		}
