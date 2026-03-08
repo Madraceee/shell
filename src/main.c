b5fe985a87edd372b5d08a36c8f4ebd3de5460a7
madraceee <raajanitheesh@gmail.com>
2026-03-08 16:56:45 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,14 +6,19 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 	while(1){
-		char *input = (char*)malloc(sizeof(char) * 500);
+		char* input = (char*)malloc(sizeof(char) * 500);
 		// TODO: Uncomment the code below to pass the first stage
 		printf("$ ");
-		scanf("%s",input);
+		fgets(input, 500, stdin);
+		input[strlen(input)-1] = '\0';
 		if(strcmp(input,"exit") == 0){
 			break;
+		}else if(strncmp(input,"echo ",5) == 0){
+			printf("%s\n", input + 5);
+		}else {
+			printf("%s: command not found\n", input);
 		}
-		printf("%s: command not found\n", input);
+		free(input);
 	}
 
   return 0;
